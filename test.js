77f3b35b0c4199a2059e9181e8988efc7193380d
Andrew Weber <zzambushzz@gmail.com>
2014-03-22 22:33:51 -0400

    Playing with casperjs and phantomjs

---
+++ test.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ambush
+ * Date: 3/22/14
+ * Time: 10:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+casper.test.begin("Hello, Test!", 1, function(test) {
+    test.assert(true);
+    test.done();
+});
+
